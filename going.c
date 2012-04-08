d2f50965c2e11e9f10fe75cdf387ae2fadc18560
Eivind Uggedal <eivind@uggedal.com>
2012-04-08 02:23:38 +0200

    First stab of reading configuration files.

--- going.c
+++ going.c
@@ -11,9 +11,11 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 
+static const char CMD_KEY[] = "cmd";
+
 struct Child {
-  char *name;
-  char *cmd;
+  char name[32];
+  char cmd[128];
   pid_t pid;
   struct Child *next;
   // TODO: add respawn counter/timer
@@ -27,41 +29,65 @@ static int only_files_selector(const struct dirent *d) {
   return strcmp(d->d_name, ".") != 0 && strcmp(d->d_name, "..") != 0;
 }
 
-void parse_config(const char *path) {
+void parse_config(const char *dirpath) {
   struct Child *prev_ch = NULL;
   struct dirent **dirlist;
   int dirn;
+  char path[PATH_MAX + 1];
+  FILE *fp;
+  char buf[256], *line, *key, *value;
 
-  dirn = scandir(path, &dirlist, only_files_selector, alphasort);
+  dirn = scandir(dirpath, &dirlist, only_files_selector, alphasort);
   if (dirn < 0) {
     // TODO: Log error.
     exit(EX_OSFILE);
-  } else {
-    while (dirn--) {
-      printf("config: %s\n", dirlist[dirn]->d_name);
-      // TODO: Read config file.
-      free(dirlist[dirn]);
-    }
-    free(dirlist);
   }
 
-  // TODO: actual parsing
-  for (int i = 0; i < 5; i++) {
-    struct Child *ch = malloc(sizeof(struct Child));
-    // TODO: check that we got memory on the heap.
+  while (dirn--) {
+    snprintf(path, PATH_MAX + 1, "%s/%s", dirpath, dirlist[dirn]->d_name);
 
-    ch->name = "echo";
-    ch->cmd = "./test/echo.sh";
+    if ((fp = fopen(path, "r")) == NULL) {
+      // TODO: Log error.
+      break;
+    }
+
+    struct Child *ch = malloc(sizeof(struct Child));
+    bool valid = false;
+    // TODO: Check that we got memory on the heap.
+
+    // TODO: Check that d_name fits.
+    strncpy(ch->name, dirlist[dirn]->d_name, sizeof(ch->name) -1);
     ch->pid = 0;
     ch->next = NULL;
 
-    if (prev_ch) {
-      prev_ch->next = ch;
-    } else {
-      head_ch = ch;
+    while ((line = fgets(buf, sizeof(buf), fp)) != NULL) {
+      key = strsep(&line, "=");
+      value = strsep(&line, "\n");
+      if (key != NULL && value != NULL) {
+        if (strcmp(CMD_KEY, key) == 0 && strnlen(value, 1) == 1) {
+          // TODO: Check that value fits.
+          strncpy(ch->cmd, value, sizeof(ch->cmd) -1);
+          valid = true;
+        }
+      }
     }
-    prev_ch = ch;
+
+    if (valid) {
+      if (prev_ch) {
+        prev_ch->next = ch;
+      } else {
+        head_ch = ch;
+      }
+      prev_ch = ch;
+    } else {
+      free(ch);
+    }
+
+    fclose(fp);
+    free(dirlist[dirn]);
   }
+
+  free(dirlist);
 }
 
 void spawn_child(struct Child *ch) {
@@ -140,7 +166,12 @@ int main(void) {
   sigaddset(&block_mask, SIGHUP);
   sigprocmask(SIG_BLOCK, &block_mask, &orig_mask);
 
-  for (ch = head_ch; ch; ch = ch->next) {
+  // TODO: What to do if we have no valid children?
+  //       - should log this.
+  //       - with inotify an empty directory can be valid.
+
+  for (ch = head_ch; ch != NULL; ch = ch->next) {
+    printf("%s", ch->name);
     spawn_child(ch);
     // TODO: Remove debug printf():
     printf("spawned: %s (cmd: %s) (pid: %d)\n", ch->name, ch->cmd, ch->pid);
@@ -156,8 +187,7 @@ int main(void) {
     respawn();
   }
 
-  // TODO: Kill and reap children if we have left the SIGCHLD loop
-  //       or just let them become zombies?
+  // TODO: Kill and reap children if we have left the SIGCHLD loop.
 
   return EXIT_SUCCESS;
 }
